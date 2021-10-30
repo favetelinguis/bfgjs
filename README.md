06c741acbec035b1ae5d6e8976c06001ddc5be87
Henrik Larsson <reg@henkelarsson.se>
2021-10-30 13:10:16 +0200

    Initial react express setup

---
+++ README.md
@@ -0,0 +1,2 @@
+Initial project setup from: https://levelup.gitconnected.com/how-to-render-react-app-using-express-server-in-node-js-a428ec4dfe2b
+Also added a proxy for react devserver
---
+++ server/index.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
+
+// Put after all routes, is the catch all?
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+})
